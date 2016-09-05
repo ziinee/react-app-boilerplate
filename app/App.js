2d52c32bf5037b9729113787eff6360c13ddfba3
Kim Jin Hee <go.jink.kr@gmail.com>
2016-09-05 17:39:25 +0900

    칸반보드 앱 만들기

--- app/App.js
+++ app/App.js
@@ -1,30 +1,40 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import KanbanBoard from './KanbanBoard';
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id:3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  },
+];
+
+render(<KanbanBoard cards={cardsList} />, document.getElementById("root"));
 
 
-// 부모 컴포넌트
-class GroceryList extends Component {
-  render () {
-    return(
-      <ul>
-        <ListItem quantity="1">Bread</ListItem>
-        <ListItem quantity="6">Eggs</ListItem>
-        <ListItem quantity="2">Milk</ListItem>
-      </ul>
-    );
-  }
-}
-
-// 자식 컴포넌트
-class ListItem extends Component {
-  render () {
-    return(
-      <li>
-        {this.props.quantity} x {this.props.children}
-      </li>
-    );
-  }
-}
-
-
-render(<GroceryList />, document.getElementById("root"));
