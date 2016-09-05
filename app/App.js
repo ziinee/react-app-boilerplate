70a9e9eb9849b1f6b7eff6112cccb9c8c86cd5d3
Kim Jin Hee <go.jink.kr@gmail.com>
2016-09-05 20:14:35 +0900

    칸반앱: 마크다운 렌더링 사용하기

--- app/App.js
+++ app/App.js
@@ -6,31 +6,20 @@ let cardsList = [
   {
     id: 1,
     title: "Read the Book",
-    description: "I should read the whole book",
+    description: "I should read the **whole** book",
     status: "in-progress",
     tasks: []
   },
   {
     id: 2,
     title: "Write some code",
-    description: "Code along with the samples in the book",
+    description: "Code along with the samples in the book.\n" +
+    "The complete source can be found at [github](https://github.com/pro-react)",
     status: "todo",
     tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
-        id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id:3,
-        name: "My own experiments",
-        done: false
-      }
+      { id: 1, name: "ContactList Example", done: true },
+      { id: 2, name: "Kanban Example", done: false },
+      { id: 3, name: "My own experiments", done: false }
     ]
   },
 ];
